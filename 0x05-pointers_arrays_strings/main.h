8bf58fa937aafabb8adb577a72d3a62901aad497
Nassiri94 <5starshop00@gmail.com>
2023-06-26 09:58:38 -0700

    task0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include "_putchar.c"
-#include <stdio.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -14,4 +10,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#endif
